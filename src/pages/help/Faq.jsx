60f32a08fb9e9026d2fb2c22d4fb63736fbb3a76
Blackxok <nasridoni1@gmail.com>
2023-12-10 22:29:02 +0500

    cng windan keyin

---
+++ src/pages/help/Faq.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Faq() {
+   return (
+      <div>
+         <h1>FAQ</h1>
+         <hr />
+         <ol>
+            <li>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Corporis, quam iure dolorem soluta nihil cupiditate? Ratione
+               temporibus recusandae unde magni?
+            </li>
+            <li>
+               Ab aperiam delectus temporibus culpa. Architecto laudantium ad
+               cupiditate tenetur in, sit atque! Ratione inventore deserunt
+               sapiente nostrum assumenda reprehenderit!
+            </li>
+            <li>
+               Dignissimos quas cumque officia sunt, provident aliquam
+               asperiores dolorem quae esse harum nostrum doloribus
+               exercitationem culpa sed soluta. Veniam, atque?
+            </li>
+            <li>
+               Recusandae, amet praesentium ipsum expedita est ab voluptas!
+               Iusto voluptatibus repudiandae quas voluptatum! Omnis ea tempora
+               dolore, impedit ratione hic.
+            </li>
+            <li>
+               Veniam harum totam alias et ut molestiae ipsum est iste, cum
+               officiis quam quod commodi sint expedita optio consequuntur? Quo.
+            </li>
+         </ol>
+      </div>
+   );
+}
+
+export default Faq;
